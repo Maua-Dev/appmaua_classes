ca6b98ba2040804de25402bd8f55b2fbba4ae6f0
Renan Reschke <renanreschke@hotmail.com>
2022-06-02 19:32:34 -0300

    classe student.py

---
+++ src/domain/entities/student.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, validator
+
+from src.helpers.errors.domain_errors import EntityError
+
+
+class Student (BaseModel):
+    name: str
+    ra: str
+
+    @validator('name')
+    def name_not_empty(cls, v: str) -> str:
+        if len(v) == 0:
+            raise EntityError('name')
+        return v.title()
+
+    @validator('ra')
+    def ra_is_valid(cls, v: str) -> str:
+        if len(v) != 8:
+            raise EntityError('ra')
+        return v
