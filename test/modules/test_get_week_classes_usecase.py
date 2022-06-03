21e1cf1275d02e36621686aa71133c17c4678807
Renan Reschke <renanreschke@hotmail.com>
2022-06-03 18:13:36 -0300

    usecase

---
+++ test/modules/test_get_week_classes_usecase.py
@@ -0,0 +1,21 @@
+import pytest
+from typing import List
+
+from src.domain.entities._class import Class
+from src.infra.repositories.class_repository_mock import ClassRepositoryMock
+from src.modules.get_week_classes.get_week_classes_usecase import GetWeekClassesUsecase
+
+
+class Test_GetWeekClassesUsecase:
+
+    @pytest.mark.asyncio
+    async def test_usecase(self):
+        repo = ClassRepositoryMock()
+        uc = GetWeekClassesUsecase(repo)
+
+        classes = await uc("19020090")
+
+        assert len(classes) == 5
+        assert type(classes) is list
+        assert type(classes[0]) is Class
+
