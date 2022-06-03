7d67398a77e69e0c2b6655619d9364333aeb0ca9
Renan Reschke <renanreschke@hotmail.com>
2022-06-02 21:18:48 -0300

    teste unitario student

---
+++ test/domain/entities/test_student.py
@@ -0,0 +1,24 @@
+import pytest
+
+from src.domain.entities.student import Student
+from src.helpers.errors.domain_errors import EntityError
+
+
+class Test_Student:
+
+    def test_student(self):
+        student = Student("Renan scheidt reschke", "19020090")
+
+        assert student.name == "Renan Scheidt Reschke"
+
+    def test_student_entity_error1(self):
+        with pytest.raises(EntityError):
+            Student("", "19020090")
+
+    def test_student_entity_error2(self):
+        with pytest.raises(EntityError):
+            Student("Renan scheidt reschke", "")
+
+    def test_student_entity_error3(self):
+        with pytest.raises(EntityError):
+            Student("Renan scheidt reschke", "1902009000000")
