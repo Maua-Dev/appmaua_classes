8d2b9eeba6736c8c12476fc39cfee06cfc3a874b
Renan Reschke <renanreschke@hotmail.com>
2022-06-29 19:52:32 -0300

    Presenter

---
+++ src/modules/get_week_classes/get_week_classes_presenter.py
@@ -0,0 +1,17 @@
+from src.helpers.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+from src.infra.repositories.class_repository_dynamo import ClassRepositoryDynamo
+from src.infra.repositories.class_repository_mock import ClassRepositoryMock
+from .get_week_classes_usecase import GetWeekClassesUsecase
+from .get_week_classes_controller import GetWeekClassesController
+
+async def lambda_handler(event, context):
+    repo = ClassRepositoryDynamo()
+    #repo = ClassRepositoryMock()
+    usecase = GetWeekClassesUsecase(repo)
+    controller = GetWeekClassesController(usecase)
+
+    httpRequest = LambdaHttpRequest(data=event)
+    response = await controller(httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+
+    return httpResponse.toDict()
